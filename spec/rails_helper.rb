6c2f150bb54d3f898428266d94523997f5154360
BaoKhanh99 <tlbk1999@gmail.com>
2022-04-04 08:48:38 +0700

    RSpec - User Controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,8 @@ require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require "rspec/rails"
-require "support/factory_bot"
+
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
