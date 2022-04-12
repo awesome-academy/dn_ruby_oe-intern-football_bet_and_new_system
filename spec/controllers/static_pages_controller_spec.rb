c5d5578dd309266699af16f0af623038e1de4a2c
BaoKhanh99 <tlbk1999@gmail.com>
2022-04-13 06:43:01 +0700

    Rspec for Ransack

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,9 +2,15 @@ require "rails_helper"
 
 RSpec.describe StaticPagesController, type: :controller do
   describe "GET home" do
-    it "render home" do
-      get :home
-      expect(response).to render_template(:home)
+    let!(:soccer_match1) { create(:soccer_match) }
+    context "finds specific soccer match" do
+      before { get :home, params: {teams_cont: "Arsenal"}}
+      it "should find soccer match by team name" do
+        expect(assigns(:soccer_matches).first).to eq soccer_match1
+      end
+
+      it { should respond_with(:success) }
+      it { should render_template(:home) }
     end
   end
   describe "GET help" do
---
+++ spec/factories/currencies.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :currency do
+    amount {500}
+    user_id {1}
+    currency_type_id {3}
+    created_at {"2022-04-08 13:13:04.641821"}
+    updated_at {"2022-04-08 13:13:04.641821"}
+  end
+end
